78f7057a1bf427c418e9bc52e8961fb3ead505f9
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2019-02-18 17:52:04 -0500

    premierCommit

---
+++ js/competence.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#F9ECEB",
+    lineWidth: 10,
+    lineCap: 'butt',
+    barColor: '#E0928E',
+    trackColor: "#F9ECEB",
+    size: 160,
+    animate: 500
+  });
+});
---
+++ js/menu.js
@@ -0,0 +1,16 @@
+
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+var sections = document.getElementsByClassName("strike");
+
+for(i=0; i < sections.length; i++){
+	sections[i].addEventListener('click',function(){
+  burgerMenu.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+})
+}
+
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
