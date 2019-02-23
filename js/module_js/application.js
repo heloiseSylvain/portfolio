6246ed6bffa9eb37d93c26473cbd179d3460b7d1
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2019-02-23 13:54:47 -0500

    scroll to top^

---
+++ js/module_js/application.js
@@ -0,0 +1,30 @@
+
+import {AnimationMenu} from './AnimationMenu.js' //
+
+
+/* l'élement de la page qui contiendra les éléments créés dynamiquement */
+let elmHeader = document.querySelector('')
+const lesOnglets = Array("Accueil","À propos", "compétences", "gallerie", "contacter")
+new AnimationMenu(lesOnglets, elmHeader, finAnim)
+
+// debutQuestionnaire()
+
+
+function finAnim()
+{
+	console.log('animation terminée')
+}
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
