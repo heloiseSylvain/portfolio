5c17b0be4ca128fa3a7a9aab175c0cde2a9acae2
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2019-03-12 12:31:46 -0400

    lkgfdhj

--- js/gallerie.js
+++ js/gallerie.js
@@ -41,8 +41,6 @@
                 $('#descProjet').text('Étant passionnée de design web, j\'ai décidé d\'orienter mon projet de fin d’études sur l\'apprentissage d’un logiciel de prototypage que nous n’avions pas utilisé au cours du programme, c’est-à-dire Figma.'); 
                 $('#boutonProjet').attr('href','maquettes.html'); 
                 $('#imgProjet').attr('src','images/figmaLarge.png');
-                $('#imgProjet').hide();
-                $('#maquette').show();
                 break;	
 
             case 'projet4': 
@@ -83,7 +81,6 @@
         $('.port, .gallery ul').removeClass('item_open');
         $('#imgProjet').removeClass('plannificateurImg');
         $('#vimeo').hide();
-        $('#maquette').hide();
         return false;
     });
 
