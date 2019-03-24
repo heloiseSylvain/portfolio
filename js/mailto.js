f78d74f3c7419dafe6c7d17b34c84a7ffb73a9ea
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2019-03-24 12:53:36 -0400

    mail

--- js/mailto.js
+++ js/mailto.js
@@ -1,14 +1,30 @@
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
 
-$('#btnCourriel').click(function() {
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "template_fsjGnZhY";
+
+  myform.find("button").text("ENVOIE EN COURS");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("Votre message a bien été envoyé!");
+       myform.find("button").text("ENVOYER");
+    }, function(err) {
+       alert("Votre message n'a pas été envoyé!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("ENVOYER");
+    });
+  return false;
+});
+
+/*$('#btnCourriel').click(function() {
 
         
         var body_message = document.getElementById('textarea').value;
-        var email = 'helo_sylvain@hotmail.com';
         var subject = (document.getElementById('nomCourriel').value + ' ' + document.getElementById('prenomCourriel').value );
+        $('#btnCourriel').attr('href', 'mailto:helo_sylvain@hotmail.com?subject='+subject+'&body='+body_message);
+        
+    
 
-    var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + body_message;
-
-    win = window.open(mailto_link, 'emailWindow');
-    if (win && win.open && !win.closed) win.close();
-
-});
+});*/
